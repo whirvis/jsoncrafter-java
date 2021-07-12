b678e0e4f0d487d72e6a44f2674c94f5d2b160b0
Trent Summerlin <whirvis@ardenus.org>
2021-07-12 18:47:04 -0500

    Fix broken JavaDoc reference in package-info.java

--- src/main/java/net/whirvis/mc/jsoncrafter/java/package-info.java
+++ src/main/java/net/whirvis/mc/jsoncrafter/java/package-info.java
@@ -2,6 +2,6 @@
  * A library used to generate rich text in Minecraft.
  * 
  * @see net.whirvis.mc.jsoncrafter.java.RichText RichText
- * @see com.whirvis.mc.text.java.TextEvent TextEvent
+ * @see net.whirvis.mc.jsoncrafter.java.event.TextEvent TextEvent
  */
 package net.whirvis.mc.jsoncrafter.java;

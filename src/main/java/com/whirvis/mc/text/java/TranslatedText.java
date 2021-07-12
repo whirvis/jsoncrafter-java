1321f8e0c992e4e866924d694578a7f147d496ec
Trent Summerlin <whirvis@ardenus.org>
2021-07-12 14:56:51 -0500

    Add fleshed out JavaDocs for TranslatedText.java

--- src/main/java/com/whirvis/mc/text/java/TranslatedText.java
+++ src/main/java/com/whirvis/mc/text/java/TranslatedText.java
@@ -8,7 +8,28 @@ import com.google.gson.JsonObject;
 
 /**
  * A container for translated Minecraft text.
- *
+ * <p>
+ * Translated text works by specifying a translation key (which can be found in
+ * the game's JSON language files), and then specifying "with" values. These are
+ * values which are not tied to a language, but rather context for the message.
+ * <p>
+ * For example, when a player joins the server, the following is shown:
+ * {@code [Player] joined the game}. The raw JSON text of this message in
+ * English is {@code %s joined the game}. By setting the "with" value to
+ * {@code Whirvis}, the output becomes: {@code Whirvis joined the game}.
+ * <p>
+ * We can accomplish translation with the following code:
+ * 
+ * <pre>
+ * TranslatedText text = new TranslatedText("multiplayer.player.joined");
+ * text.setWith("Whirvis");
+ * System.out.println(text.toString());
+ * </pre>
+ * 
+ * Depending on the client language, the message could be:
+ * {@code "Whirvis joined the game"}, {@code "Whirvis a rejoint la partie"},
+ * {@code "Whirvis se ha unido a la partida"}, etc.
+ * 
  * @see PlainText
  * @see KeybindText
  */
@@ -22,7 +43,8 @@ public class TranslatedText extends MinecraftText {
 	 * @param translate
 	 *            the translation key.
 	 * @param with
-	 *            the parameters to format this text with.
+	 *            the parameters to format this text with. Values will be
+	 *            converted to JSON via {@link #toJsonElement(Object)}.
 	 * @throws NullPointerException
 	 *             if {@code translate} is {@code null}.
 	 */
@@ -35,10 +57,14 @@ public class TranslatedText extends MinecraftText {
 	 * Sets the parameters to format this text with.
 	 * 
 	 * @param with
-	 *            the parameters to format this text with.
+	 *            the parameters to format this text with. Values will be
+	 *            converted to JSON via {@link #toJsonElement(Object)}.
+	 * @return this text.
 	 */
-	public void setWith(@Nullable Object... with) {
+	@Nonnull
+	public TranslatedText setWith(@Nullable Object... with) {
 		this.with = with;
+		return this;
 	}
 
 	/**
@@ -47,12 +73,15 @@ public class TranslatedText extends MinecraftText {
 	 * @param translate
 	 *            the translation key.
 	 * @param with
-	 *            the parameters to format this text with.
+	 *            the parameters to format this text with. Values will be
+	 *            converted to JSON via {@link #toJsonElement(Object)}.
+	 * @return this text.
 	 */
-	public void setTranslation(@Nullable String translate,
+	public TranslatedText setTranslation(@Nullable String translate,
 			@Nullable Object... with) {
 		this.setContent(translate);
 		this.setWith(with);
+		return this;
 	}
 
 	@Override
